776f35f8886f474126d84afa977e3c7e9dea4fc3
Samantha Yan <yyan30@my.bcit.ca>
2024-05-10 12:10:20 -0700

    buttons

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
-const shirts = ["shirt1","shirt2","shirt3","shirt4", "shirt5"]
+const bodyColors = ["black", "#000066", "tomato", "#993300", "#5c5c3d"];
+const earColors = ["black", "pink", "gray", "white", "#993300"];
+const eyeColors = ["black", "#99ccff", "#ff999f", "#993300"];
+const tailColors = ["black", "gray", "pink", "#993300", "orange"];
+const legColors = ["black", "#5c5c3d", "#993300", "#000066"];
+const faceColors = ["black", "#5c5c3d", "#993300", "#000066"];
+/*
+const accessories = [
+    "url_to_acc1_image",
+    "url_to_acc2_image",
+    "url_to_acc3_image",
+    "url_to_acc4_image"
+]; */
 
-const bottoms = ["bottom1","bottom2","bottom3","bottom4", "bottom5"]
 
-const accessories = ["acc1","acc2","acc3","acc4", "acc5"]
+//
+function changeEars() {
+    let currentIndex = Math.floor(Math.random() * earColors.length);    
+    document.querySelectorAll(".left-ear, .right-ear").forEach((ear) => {
+    ear.style.borderColor = earColors[currentIndex];
+    });
+}
+
+
+function changeTail() {
+    let currentIndex = Math.floor(Math.random() * tailColors.length);
+    document.querySelectorAll(".tail").forEach((tail) => {
+        tail.style.backgroundColor = tailColors[currentIndex];
+        });
+}
+
+function changeEyes() {
+    let currentIndex = Math.floor(Math.random() * earColors.length);    
+    document.querySelectorAll(".left-inner-eye, .right-inner-eye").forEach((eyes) => {
+    eyes.style.backgroundColor = eyeColors[currentIndex];
+    });
+}
+
+function changeFace() {
+    let currentIndex = Math.floor(Math.random() * earColors.length);    
+    document.querySelectorAll(".cat-head").forEach((face) => {
+    face.style.backgroundColor = faceColors[currentIndex];
+    face.style.borderColor = faceColors[currentIndex];
+    });
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
