f87dccbb948c53a4c0cee36f6937bf1cc48b3ac1
Samantha Yan <yyan30@my.bcit.ca>
2024-05-10 12:31:09 -0700

    adding buttons

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const bodyColors = ["black", "#000066", "tomato", "#993300", "#5c5c3d"];
 const earColors = ["black", "pink", "gray", "white", "#993300"];
 const eyeColors = ["black", "#99ccff", "#ff999f", "#993300"];
 const tailColors = ["black", "gray", "pink", "#993300", "orange"];
-const legColors = ["black", "#5c5c3d", "#993300", "#000066"];
+const frontLegColors = ["black", "#5c5c3d", "#993300", "#000066"];
+const backLegColors = ["black", "#5c5c3d", "#993300", "#000066"];
 const faceColors = ["black", "#5c5c3d", "#993300", "#000066"];
 /*
 const accessories = [
@@ -37,14 +38,33 @@ function changeEyes() {
 }
 
 function changeFace() {
-    let currentIndex = Math.floor(Math.random() * earColors.length);    
+    let currentIndex = Math.floor(Math.random() * faceColors.length);    
     document.querySelectorAll(".cat-head").forEach((face) => {
     face.style.backgroundColor = faceColors[currentIndex];
     face.style.borderColor = faceColors[currentIndex];
     });
 }
 
+function changeBody() {
+    let currentIndex = Math.floor(Math.random() * bodyColors.length);    
+    document.querySelectorAll(".cat-body").forEach((body) => {
+    body.style.backgroundColor = bodyColors[currentIndex];
+    });
+}
 
+function changeBackLegs() {
+    let currentIndex = Math.floor(Math.random() * backLegColors.length);    
+    document.querySelectorAll(".foot").forEach((backlegs) => {
+    backlegs.style.backgroundColor = backLegColors[currentIndex];
+    });
+}
+
+function changeFrontLegs() {
+    let currentIndex = Math.floor(Math.random() * frontLegColors.length);    
+    document.querySelectorAll(".arm").forEach((frontLegs) => {
+    frontLegs.style.backgroundColor = frontLegColors[currentIndex];
+    });
+}
 
 
 
